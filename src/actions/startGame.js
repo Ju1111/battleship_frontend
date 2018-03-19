9b7d0762a3049166aac267e0411f5a2bb77bb33a
Ju111 <SchneiderJu@gmx.net>
2018-03-19 16:59:06 +0100

    component folders and create game action

---
+++ src/actions/startGame.js
@@ -0,0 +1,15 @@
+export const createGame = () => (dispatch, getState) => {
+  const state = getState()
+  const jwt = state.currentUser.jwt
+
+  request
+    .post(`${baseUrl}/games`)
+    .set('Authorization', `Bearer ${jwt}`)
+    .then(result => {
+      dispatch({
+        type: ADD_GAME,
+        payload: result.body
+      })
+    })
+    .catch(err => console.error(err))
+}
