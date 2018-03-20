98ff115fe9d5a95fbe88f9dad9b1d8f9f8e25c67
Ju111 <SchneiderJu@gmx.net>
2018-03-20 17:33:29 +0100

    click on square and dispatch action while position of square is known

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,3 +1,5 @@
+import { MAKE_GUESS } from '../actions/types'
+
 const initialState = {
   board1:[
     ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
@@ -26,5 +28,25 @@ const initialState = {
 }
 
 export default (state = initialState, { type, payload } = {}) => {
-  return state
+  switch(type) {
+    case MAKE_GUESS:
+      return {...state,board2:state.board2.map((row, rowIndex) =>{
+        if (rowIndex !== payload.row) return row
+
+        return row.map((value, colIndex) => {
+          if (colIndex !== payload.col) return value
+
+          switch(value) {
+            case '0': return '0x'
+            case '1': return '1x'
+            case '2': return '2x'
+            case '3': return '3x'
+            case '4': return '4x'
+            case '5': return '5x'
+            default: return '0'
+          }
+        })
+      })}
+    default: return state
+  }
 }
