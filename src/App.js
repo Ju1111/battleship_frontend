0002d89d60322af5f6bc1f6078bdf61f1a2a94bc
Julia Schneider <SchneiderJu@gmx.net>
2018-03-22 10:51:25 +0100

    Update App.js
    deleted signup page from routers since it's part of the login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import PlaceShips from './components/ships/ShipsContainer'
 import GameBattle from './components/game/GameContainer'
 import GamesList from './components/game/GamesList'
 import LoginPage from './components/login/LoginPage'
-import SignupPage from './components/signup/SignupPage'
 import LogoutPage from './components/logout/LogoutPage'
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
@@ -20,7 +19,6 @@ class App extends Component {
           </header>
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/logout" component={LogoutPage} />
-          <Route exact path="/signup" component={SignupPage} />
           <Route exact path="/games" component={GamesList} />
           <Route exact path="/games/:id/ships" component={PlaceShips} />
           <Route exact path="/games/:id" component={GameBattle} />
