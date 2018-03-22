f8c0d69ef58c19812742ea74339baed62a526c9a
Safisyx <safisyx@gmail.com>
2018-03-22 10:59:42 +0100

    Just play a little with the App.js and then clean it up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Board from './components/game/Board'
-import GuessBoard from './components/game/GuessBoard'
-import PlaceShips from './components/ships/ShipsContainer'
+
 
 class App extends Component {
   render() {
@@ -11,10 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Battleship</h1>
         </header>
-        <div className="Boards">
-          <Board />
-          <GuessBoard/>
-        </div>
+
       </div>
     );
   }
