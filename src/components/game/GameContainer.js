2fc8d56e045228cf78184dd616c2c841005e36b7
Safisyx <safisyx@gmail.com>
2018-07-03 15:31:55 +0200

    Give player the right boards (only guesses)

--- src/components/game/GameContainer.js
+++ src/components/game/GameContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import {Redirect} from 'react-router-dom'
-import {getBoard, joinGame, updateGame, getGames} from '../../actions/games'
+import {getBoard, updateGame, getGames} from '../../actions/games'
 import {getUsers} from '../../actions/users'
 import {userId} from '../../jwt'
 import Board from './Board'
@@ -12,9 +12,6 @@ import './GameContainer.css'
 import BackButton from '../assets/GoBackButton'
 
 class GameBattle extends Component {
-
-  //here or in ShipsContainer or both?
-  joinGame = () => this.props.joinGame(this.props.game.id)
 
   makeMove = (toRow, toCell) => {
     const {game, updateGame} = this.props
@@ -37,10 +34,11 @@ class GameBattle extends Component {
   }
 
   render() {
-    const {game, users, authenticated, userId} = this.props
+    const {game, users, authenticated, userId, board} = this.props
     console.log('==================');
-    console.log(game);
-    if (game === null || users === null) return 'Loading...'
+    console.log(board.game, this.props.match.params.id);
+    if (game === null || users === null || board.game !== this.props.match.params.id) return 'Loading...'
+    if (game && game.players.length<2) return 'Loading...'
 
     if (!authenticated) return (
 			<Redirect to="/login" />
@@ -54,33 +52,22 @@ class GameBattle extends Component {
   			<Redirect to={`/games/${game.id}/ships`} />
   		)
 
+    let board1, board2
+
+    if (player && (player.symbol==='2')) {
+      board1=game.board2
+      board2=game.board1
+    }
+    else {
+      board1 = game.board1
+      board2 = game.board2
+    }
+
     return (
-      // <div>
-      // <div class="outer-paper">
-      //   <h1>Game #{game.id}</h1>
-      //
-      //   <p>Status: {game.status}</p>
-      //
-      //   {
-      //     game.status === 'started' &&
-      //     player && player.symbol === game.turn &&
-      //     <div>Its your turn!</div>
-      //   }
-      //
-      //   {
-      //     game.status === 'pending' &&
-      //     game.players.map(p => p.userId).indexOf(userId) === -1 &&
-      //     <button onClick={this.joinGame}>Join Game</button>
-      //   }
-      //
-      //   <hr />
-      //
-      //   {game.board.map(this.renderRow)}
-      // </div>)
       <div>
         <div className="Boards">
-          <Board board={this.props.boards.board}/>
-          <GuessBoard player={player} board={this.props.boards.guessBoard}/>
+          <Board board={board1}/>
+          <GuessBoard player={player} board={board2}/>
         </div>
         <div className="GoBack">
           <BackButton />
@@ -94,12 +81,12 @@ const mapStateToProps = (state, props) => ({
   authenticated: state.currentUser !== null,
   userId: state.currentUser && userId(state.currentUser.jwt),
   game: state.games && state.games[props.match.params.id],
-  boards: state.boards,
+  board: state.board,
   users: state.users
 })
 
 const mapDispatchToProps = {
-  getBoard, getGames, getUsers, joinGame, updateGame
+  getBoard, getGames, getUsers, updateGame
 }
 
 export default withRouter(
